f422281d8f2ca20ddddf773aa6c86b187fa7616c
Matthew Miskovsky <misko@Matthews-Air.lan>
2022-11-04 23:16:34 -0400

    added database connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const app = express()
+require('dotenv').config({path: './config/.env'});
+const connectDB = require('./config/db')
+const User = require('./models/User')
 
-app.listen(2121,  () =>{
-    console.log('Server is running ')
+connectDB()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(process.env.PORT,  () =>{
+    console.log(`${process.env.PORT}`)
 })
 
 app.get('/home', (req, res) => {
     res.status(200).json({bestDogs: ['Ozzy', 'Bella', 'Willow', 'Tito']})
+})
+
+app.post('/register', async (req, res) => {
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    res.status(200).json(user)
+})
+
+app.get('/getUsers', async (req, res) => {
+    const user = await User.find()
+    res.status(200).json(user)
 })
