7f7370bba417d68c4f1b1749b432c874d551f474
Matthew Miskovsky <matt.miskovsky@gmail.com>
2022-11-11 22:31:10 -0500

    add new elements to loaded dashboard for active medication and adding medication

--- frontend/src/components/LoadedDashboard.js
+++ frontend/src/components/LoadedDashboard.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Doctor from '../images/dog-medicine.png'
 
 function LoadedDashboard() {
+  
+  const [needMeds, setNeedMeds] = useState(null)
+
+  function addMeds(){
+    setNeedMeds(true)
+  }
+
+  const [formData, setFormData] = useState({
+    medicationName: '',
+    dosage: '',
+    timesPerDay: ''
+  }) 
+
+  function onChange(e){
+    setFormData((prevValue)=>{
+      return{
+        ...prevValue,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  async function postMedication(e) {
+    e.preventDefault()
+    try {
+      const formInfo = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({medicationName: formData.medicationName, dosage: formData.dosage, timesPerDay: formData.timesPerDay})
+    }
+        const response = await fetch('/dashboard/addFirstMedication', formInfo)
+        const data = await response.json()
+        setNeedMeds(null)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="mainDashboardContainer">
         <header className="dashboardHeader" style={{
@@ -14,6 +52,56 @@ function LoadedDashboard() {
             <p>Seizure tracking manager</p>
             <button className="startTrackingBtn">Start Tracking</button>
         </header>
+        <div className="dashboardMedicationContainer">
+        <section className="medicationStatus">
+            <h2>Ozzy's Medication(s)</h2>
+            <p>Keppra 200mg 2 time(s) per day.</p>
+            <p>Keppra 200mg 2 time(s) per day.</p>
+            <p>Keppra 200mg 2 time(s) per day.</p>
+            <p>Keppra 200mg 2 time(s) per day.</p>
+        </section>
+        <section className="addMedication">
+            {needMeds && (
+                <>
+                    <form className="addOneMed" onSubmit={postMedication}>
+                      <h1>Add a medication below!</h1>
+                      <input
+                      className="medicationInput" 
+                      type="text"
+                      name="medicationName"
+                      placeholder="Your dog's medication"
+                      value={formData.medicationName}
+                      onChange={onChange}
+                      />
+                      <input
+                      className="medicationInput" 
+                      type="text"
+                      name="dosage"
+                      placeholder="Dosage (Example: 120mg)"
+                      value={formData.dosage}
+                      onChange={onChange}
+                      />
+                      <input
+                      className="medicationInput" 
+                      type="text"
+                      name="timesPerDay"
+                      placeholder="Frequency (Times per day)"
+                      value={formData.timesPerDay}
+                      onChange={onChange}
+                      />
+                      <button className="addOneMedBtn">Add</button>
+                    </form>
+                </>
+            )}
+            {!needMeds && (
+                <>
+                    <h2>Adding more medication?</h2>
+                    <p>Click below to add more medication.</p>
+                    <button className="addOneMedBtn" onClick={addMeds}>Add Meds</button>
+                </>
+            )}
+        </section>
+        </div>
     </div>
   )
 }
